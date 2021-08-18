216261257d16d0117506bf7cb3ebd122dc762843
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 19:50:16 +0000

    header.js: changed

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,8 @@ const Header = ({ siteTitle }) => (
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
-      }}
+      }
+      <h1></>}
     >
       <h1 style={{ margin: 0 }}>
         <Link
