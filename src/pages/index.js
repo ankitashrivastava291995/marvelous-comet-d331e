f1a0a7a52665214c74d0f86aa1b91a820adc0c50
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 19:56:06 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,17 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <di
     <h1>Industries We serve</h1>
     <p>Welcome to your new Gatsby site.</p>
-    
-   <StaticImage
+    <StaticImage
       src="../images/Industries.jpg"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
-    /> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+    />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
 
    
   </Layout>
