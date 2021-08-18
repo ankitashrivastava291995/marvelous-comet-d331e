23ebc204669cbe8d048ac094b4f4916f815a1172
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 19:35:39 +0000

    industries.js: changed

--- src/pages/industries.js
+++ src/pages/industries.js
@@ -1,2 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
+export default function Industries() {
+    return (
+        <div>
+            <header/>
+            <div>
+                <div>Industries</div>
+                <div>industry image</div>
+
+            </div>
+        </div>
+    )
+}
