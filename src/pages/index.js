aa6c4e472c00900c752e4302e7819ad993d8089a
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 19:58:53 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,14 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+    <StaticImage
+      src="../images/Industries.jpg"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
   </Layout>
 )
 
