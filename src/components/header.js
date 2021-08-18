98acb4b15be082d628e2414150378a29f568d69a
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 19:49:25 +0000

    header.js: changed

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#fb3f40`,
             textDecoration: `none`,
           }}
         >
