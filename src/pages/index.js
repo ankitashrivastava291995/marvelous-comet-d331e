b317dabd5a4a0d25fd203afe72c2248640e93f00
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 19:57:03 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,7 @@ const IndexPage = () => (
     />
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
     <div>
-    <div>
-    </div>
+    <div></div>
     </div>
    
   </Layout>
