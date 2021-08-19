9846f99d67514370b445b4c6c323a94674756997
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 12:26:42 +0000

    header.js: changed

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,24 @@
-
 import React from 'react'
 
-export default function header() {
+export const header = () => {
   return (
     <div className="header">
-<div>
- <ul>
- <li>
- <a href="/Home">Home</a>
- </li>
- <li>
- <a href="/industries">Industries</a>
- </li>
- <li>
- <a href="/aboutus">About Us</a>
- </li>
- <li>
- <a href="/contact">Contact Us</a>
- </li>
- </ul>
-</div>      
-    </div>
+    <div>
+     <ul>
+     <li>
+     <a href="/Home">Home</a>
+     </li>
+     <li>
+     <a href="/industries">Industries</a>
+     </li>
+     <li>
+     <a href="/aboutus">About Us</a>
+     </li>
+     <li>
+     <a href="/contact">Contact Us</a>
+     </li>
+     </ul>
+    </div>      
+        </div>
   )
 }
-export default header;
