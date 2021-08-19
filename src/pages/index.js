83139ea01bc364be1914656e760e3e58ba8453ca
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 12:59:05 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import * as React from "react"
 import header from '../components/header'
 const IndexPage = () => (
-
- <layout>
- <>
+<>
  <header/>
- <h1>ankita</h1> 
+ <h1>ankita</h1>
  </>
- </layout>
-
- 
 )
 
 export default IndexPage
