41a76d67ce8a77f70715f48f4dc07d457dc34f87
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 19:24:08 +0000

    Header.jsx: changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
      <Link href="/Home">Home</Link>
      </li>
      <li>
-     <Link href="/industries">Industries</Link>
+     <Link ="/industries">Industries</Link>
      </li>
      <li>
      <Link href="/aboutus">About Us</Link>
