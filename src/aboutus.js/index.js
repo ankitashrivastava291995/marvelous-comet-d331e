efcab1e736ee338c1a3981e619953d79413a45ea
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 19:27:12 +0000

    aboutus.js: removed

---
+++ src/aboutus.js/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+
+const IndexPage = () => (
+<>
+ <Header/>
+ <h1>ankita shrivastava</h1>
+ <Footer/>
+ </>
+)
+
+export default IndexPage
