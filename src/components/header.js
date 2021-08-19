7dab7468d35a7de40726616f7ae0c60e6df6b893
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 11:57:19 +0000

    header.js: changed

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,27 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import React from 'react'
 
+export default function header() {
+  return (
+    <div classname="header">
+      <div>
+        <ul>
+         <li>
+           <a href="/">Home</a>
+         </li> 
+         <li>
+         <a href="/industrires">Home</a>
+         </li> 
+         <li>
+         <a href="/Services">Services</a>
+         </li> 
+         <li>
+         <a href="/Aboutus">Aboutus</a>
+         </li> 
+        </ul>
+      </div>
+      
+    </div>
+  );
+}
+export default header;
+  
