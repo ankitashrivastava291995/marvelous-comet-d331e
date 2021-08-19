dadebf699cd8b650bcab82fef1ca869a22199dd1
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 20:56:36 +0000

    HomeHero.jsx: changed

--- src/components/Hero/HomeHero.jsx
+++ src/components/Hero/HomeHero.jsx
@@ -5,7 +5,7 @@ export const HomeHero = () => {
     <div>
       <StaticImage
       src="../images/Industry_1.jpg"
-      width={700}
+      width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
