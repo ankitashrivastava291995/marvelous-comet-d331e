41b05dba2236e98ae90f04534efcda04a653adb0
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 12:02:31 +0000

    header.js: changed

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,40 @@
-import React from 'react'
 
-export default function header() {
-  return (
-    <div classname="header">
-      <div>
-        <ul>
-         <li>
-           <a href="/">Home</a>
-         </li> 
-         <li>
-         <a href="/industrires">Home</a>
-         </li> 
-         <li>
-         <a href="/Services">Services</a>
-         </li> 
-         <li>
-         <a href="/Aboutus">Aboutus</a>
-         </li> 
-        </ul>
-      </div>
-      
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `#000000`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+        color: `#fb3f40`
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
     </div>
-  );
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
 }
-export default header
-  
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
