139fc0ca1f329cf5db710166d417de12cc3ea85e
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 19:44:59 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import * as React from "react"
 import layout from "../components/layout"
 
 const IndexPage = () => (
-<>
+
 <layout>
  <h1>ankita shrivastava</h1>
 </layout>
- </>
+
 )
 
 export default IndexPage
