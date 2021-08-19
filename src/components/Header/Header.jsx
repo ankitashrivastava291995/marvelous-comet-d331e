daf1f5f2951a3fec3587b24cb7841a6db2335fc9
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 13:09:14 +0000

    Header.jsx: added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="header">
+    <div>
+     <ul>
+     <li>
+     <a href="/Home">Home</a>
+     </li>
+     <li>
+     <a href="/industries">Industries</a>
+     </li>
+     <li>
+     <a href="/aboutus">About Us</a>
+     </li>
+     <li>
+     <a href="/contact">Contact Us</a>
+     </li>
+     </ul>
+    </div>      
+        </div>
+  )
+}
+export default Header
