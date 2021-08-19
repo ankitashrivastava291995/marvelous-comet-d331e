e6bc724d9608e27bdec0ab61dd370ea69706d42d
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 19:38:49 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import * as React from "react"
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
 
 const IndexPage = () => (
 <>
- <Header/>
+
  <h1>ankita shrivastava</h1>
- <Footer/>
+
  </>
 )
 
