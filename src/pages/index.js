d311641594d4294d7193978708841a7e611c13f3
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 12:08:06 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,6 @@
 import * as React from "react"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-     <callback/>
-    <h1>Industries We serve</h1>
-    <p>Welcome to erainterfaces</p>
-    <StaticImage
-      src="../images/Industries.jpg"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
 
 )
 
