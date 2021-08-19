c1461e04ae91a7c79c7d43b3bc4b21e7a089c40d
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 12:25:07 +0000

    header.js: changed

--- src/components/header.js
+++ src/components/header.js
@@ -1,51 +1,9 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
 import React from 'react'
 
-export const header = () => {
+export default function header() {
   return (
-    <div>
-       <div className="header">
+    <div className="header">
 <div>
  <ul>
  <li>
@@ -63,6 +21,6 @@ export const header = () => {
  </ul>
 </div>      
     </div>
-    </div>
   )
 }
+export default header;
