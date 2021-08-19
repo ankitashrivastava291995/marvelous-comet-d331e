9d60dd61660328952455c687385a6cc04bb5da3e
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 20:38:32 +0000

    Header.jsx: changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from 'gatsby'
 import headerStyles from './header.module.css'
 export const Header = () => {
   return (
-    <div className={headerStyles.header}>
+    <div >
       <div>ERAINTERFACES</div>
     <div>
      <ul>
