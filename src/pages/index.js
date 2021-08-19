43bbb0fb41f8720fb0075ca115b86d8a97c8ef8a
Stackbit Code Editor <projects@stackbit.com>
2021-08-19 19:21:49 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {Link} from 'gatsby'
+import {Link} from 'gatsby';
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 
