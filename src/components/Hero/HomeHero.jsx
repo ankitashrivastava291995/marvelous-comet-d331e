5977d036579263f0ae0acfa52e78a514f9f9b5ea
Stackbit Code Editor <projects@stackbit.com>
2021-08-22 20:29:29 +0000

    HomeHero.jsx: changed

--- src/components/Hero/HomeHero.jsx
+++ src/components/Hero/HomeHero.jsx
@@ -6,7 +6,7 @@ export const HomeHero = () => {
     <h1>Industries We Serve</h1>
     <div>
       <div>
-       <img src={Industry_1}" alt="Industry_1">
+       <img src={Industry_1} alt="Industry_1">
       </div>
       <div></div>
      
