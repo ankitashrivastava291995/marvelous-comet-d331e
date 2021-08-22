6896ba6d2353a81ce40d8540edde526bc3027750
Stackbit Code Editor <projects@stackbit.com>
2021-08-22 18:15:02 +0000

    Header.jsx: changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'gatsby'
-import "./header"
+import "./header.moduc.css"
 import headerStyles from './header.module.css'
 export const Header = () => {
   return (
