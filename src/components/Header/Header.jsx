677f71ef6a3f8635a22536c41fd5f5a2eafc74d9
Stackbit Code Editor <projects@stackbit.com>
2021-08-22 19:30:17 +0000

    Header.jsx: changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div className="header ">
       <div  className="logo">ERAINTERFACES</div>
-   
+    <div>
     <ul>
      <li className="li">
      <Link to="/">Home</Link>
