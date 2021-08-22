b46e4e7882ad24251307f82a4b97ffa8c78c9183
Stackbit Code Editor <projects@stackbit.com>
2021-08-22 19:38:53 +0000

    Header.jsx: changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,22 @@ export const Header = () => {
     <div className="header ">
       <div  className="logo">ERAINTERFACES</div>
     <div>
-   
+    <ul>
+     <li className="li">
+     <Link to="/">Home</Link>
+     </li>
+    <li  className="li">
+     <Link to="/industries">Industries</Link>
+     </li>
+     <li  className="li">
+     <Link to="/aboutus">About Us</Link>
+     </li>
+     <li  className="li">
+     <Link to="/contact">Contact Us</Link>
+     </li>
+     </ul>
     </div>      
-      
+        </div>
   )
 }
 export default Header
